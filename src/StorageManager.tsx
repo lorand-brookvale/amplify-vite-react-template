fdf8fe1f585c9ae2532d65fd94c9445e1f7d2b7c
Lóránd Balog <info@lorz.hu>
2024-08-01 00:04:25 +0200

    added theme

--- src/StorageManager.tsx
+++ src/StorageManager.tsx
@@ -1,14 +1,33 @@
 import { StorageManager } from '@aws-amplify/ui-react-storage';
 import '@aws-amplify/ui-react/styles.css';
+import { ThemeProvider } from '@aws-amplify/ui-react';
+
+const theme = {
+    name: 'default-theme',
+    tokens: {
+      borderWidths: {
+        small: '2px',
+      },
+      components: {
+        storagemanager: {         
+          dropzone: {   
+            borderColor: '{colors.primary.60}',
+          },
+        },
+      },
+    },
+  };
 
 function DefaultStorageManagerExample()  {
   return (
-    <StorageManager
-      acceptedFileTypes={['*/*']}
-      path="files/"
-      maxFileCount={10}
-      isResumable
-    />
+    <ThemeProvider theme={theme}>
+        <StorageManager        
+        acceptedFileTypes={['*/*']}
+        path="files/"
+        maxFileCount={10}
+        isResumable
+        />
+    </ThemeProvider>
   );
 }
 
